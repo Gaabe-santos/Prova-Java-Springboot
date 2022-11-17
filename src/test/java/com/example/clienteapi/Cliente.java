04e5dcbbfeedf45fda2974c527560a2e1727d56c
Gabriel Santos <gabriel.sts0@outlook.com>
2022-11-17 19:51:15 -0300

    Adicionando Classe Cliente

--- src/test/java/com/example/clienteapi/Cliente.java
+++ src/test/java/com/example/clienteapi/Cliente.java
@@ -1,5 +1,53 @@
 package com.example.clienteapi;
 
+import com.sun.istack.NotNull;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Cliente {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private String nome;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Endereco> enderecos;
+
+    public Cliente(String nome, String enderecos, List<Endereco>endereco){
+        this.nome = nome;
+        this.enderecos = endereco;
+    }
+
+    public Cliente(){
+
+    }
+
+    public Cliente(Long id, String nome, String endereco){
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
 }
